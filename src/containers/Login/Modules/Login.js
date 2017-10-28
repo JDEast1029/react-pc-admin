6c220512aa72e708e0f4d664802c9a00593d2a16
JDEast1029 <852778359@qq.com>
2017-10-28 15:51:11 +0800

    clear

--- src/containers/Login/Modules/Login.js
+++ src/containers/Login/Modules/Login.js
@@ -5,27 +5,10 @@ import { Link } from 'react-router';
 import * as actions from '../../../actions/login';
 import * as types from '../../../contants/actionTypes/login';
 
-import ParticleAnimate from '../../../components/_common/ParticleAnimate/ParticleAnimate';
-
 class Login extends Component {
     constructor(props) {
 		super(props);
 		this.handleLogin = this.handleLogin.bind(this);
-	}
-
-	componentDidMount() {
-		// ParticleAnimate.init({
-		// 	vx: 4,//点x轴速度,正为右，负为左
-		// 	vy:  4,//点y轴速度
-		// 	height: 2,//点高宽，其实为正方形，所以不宜太大
-		// 	width: 2,
-		// 	count: 100,//点个数
-		// 	color: "121, 162, 185",//点颜色
-		// 	stroke: "130,255,255",//线条颜色
-		// 	dist: 6000,//点吸附距离
-		// 	e_dist: 20000,//鼠标吸附加速距离
-		// 	max_conn: 10//点到点最大连接数
-		// })
 	}
 
     handleLogin() {
